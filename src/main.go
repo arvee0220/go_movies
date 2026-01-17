edc9ae20b39b779e61de894138ef0ec3da6dafab
arvee0220 <rowellverdejo@gmail.com>
2026-01-17 19:46:42 +0800

    Refactor main entry point

--- src/main.go
+++ src/main.go
@@ -5,9 +5,7 @@ import (
 	"go_movies/src/controller"
 	"go_movies/src/structs"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -15,8 +13,8 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	structs.Movies = append(structs.Movies, Movie{ID: "1", Isbn: "438227", Title: "Movie 1", Director: &Director{Firstname: "John", Lastname: "Doe"}})
-	structs.Movies = append(structs.Movies, Movie{ID: "2", Isbn: "45455", Title: "Movie 2", Director: &Director{Firstname: "Jane", Lastname: "Doe"}})
+	structs.Movies = append(structs.Movies, structs.Movie{ID: "1", Isbn: "438227", Title: "Movie 1", Director: &structs.Director{Firstname: "John", Lastname: "Doe"}})
+	structs.Movies = append(structs.Movies, structs.Movie{ID: "2", Isbn: "45455", Title: "Movie 2", Director: &structs.Director{Firstname: "Jane", Lastname: "Doe"}})
 
 	r.HandleFunc("/movies", controller.GetMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", controller.GetMovie).Methods("GET")
